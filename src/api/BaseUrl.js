bd090894fd8f7480aba67380ec63a3427f313eda
dk250720 <dk250720@gmail.com>
2024-10-04 12:50:09 +0530

    base url change

--- src/api/BaseUrl.js
+++ src/api/BaseUrl.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const BaseUrl = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://digiquestpython.pythonanywhere.com/',
 });
 
 export default BaseUrl;
